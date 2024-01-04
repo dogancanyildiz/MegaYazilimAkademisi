d720d11316632996b8e1143b6c0e8c085365e831
Doğan Can YILDIZ <developer@dogancanyildiz.com>
2024-01-04 13:55:56 +0300

    Add temperatureconverter public folder

---
+++ 06-TemperatureConverter/temperature-converter/src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/App.css';
+import TemperatureConverter from './components/TemperatureConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <TemperatureConverter />
+    </div>
+  );
+}
+
+export default App;
